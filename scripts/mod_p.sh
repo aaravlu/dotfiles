d0d992bd3f7b513398114e691288fa9a1fe2b44a
emmettlu <demolemon@outlook.at>
2026-03-31 01:16:01 +0800

    Update

--- scripts/mod_p.sh
+++ scripts/mod_p.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-[ -n "$WAYLAND_DISPLAY" ] && menu="wmenu -l 3" || menu="dmenu -l 3"
+[ -n "$WAYLAND_DISPLAY" ] && menu="wmenu -l 3 -f 14" || menu="dmenu -l 3"
 
 choice=$(printf "Logout\nReboot\nShutdown" | eval $menu)
 
